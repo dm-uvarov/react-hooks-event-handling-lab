0416394aebc5e521697805fb0fb7cf0e82a795f2
Jimmy <dm.a.uvarov@gmail.com>
2022-02-06 23:11:59 -0500

    commit tree bb0b9a127a23d0516d9562c783c32b04542bfd32

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+
+function HandleBlur() {
+    console.log("Hey! Eyes on me!")
+}
+
+function EyesOnMe() {
+    return (
+
+        <button onBlur={HandleBlur} 
+                onFocus={()=>(console.log('Good!'))} 
+        >Eyes on me</button>
+
+    )
+}
+
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,23 @@
+import React from "react";
+
 // Code Keypad Component Here
+
+
+
+function handleOnChange(event) {
+    console.log('Entering password...');
+}
+
+
+
+function Keypad() {
+    return (
+        <input
+            type="password"
+            name="enter somth"
+            placeholder="aha here"
+            onChange={handleOnChange}
+        />)
+}
+
+export default Keypad;
